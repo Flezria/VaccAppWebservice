fe4a4abbea6e28999f088a4eec4be1be7576adff
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-04-24 15:38:01 +0200

    Add project files.

---
+++ VaccAppWebservice/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VaccAppWebservice.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ VaccAppWebservice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VaccAppWebservice.WebApiApplication" Language="C#" %>
